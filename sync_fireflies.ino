38d47f891a20272ba8e64366f47b366d78a633da
My Name <my@email.com>
2017-07-25 16:42:30 +0200

    add important variables

--- sync_fireflies.ino
+++ sync_fireflies.ino
@@ -1,3 +1,12 @@
+int phi = 0; //current phase of the firefly cycle
+int x = 0; //state x --> x=f(phi) 
+int epsilon = 0.2 //coupling strength, the amount "x" gets lifted up if a flsh is received
+
+int frequency = 1 //in Hz, the maximum timer value (phi) is calculated out of the frequency
+
+int trigger_threshold = 0 //threshold after that entrainment could occur --> trigger_threshold = x_max - epsilon
+int ambient_treshold = 0 //the IR-Receiver has an internal pull-up resistor, so ambient_treshold has to be smaller if a flash is received 
+
 void void setup()
 {
 	
