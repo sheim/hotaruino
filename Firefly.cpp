807d2cf52fa257f23e66d981f80962f23f9f1cce
Jonas Lang <jonilang@gmx.de>
2017-08-09 08:56:12 +0200

    add function bodies

--- Firefly.cpp
+++ Firefly.cpp
@@ -1,3 +1,62 @@
 #include "Arduino.h"
 #include "Firefly.h"
 
+Firefly::Firefly()
+{
+
+}
+
+void Firefly::millisecondDelay(int time_delay)
+{
+
+}
+
+void Firefly::potentiometerReadIn()
+{
+
+}
+
+void Firefly::flashReceiveCheck()
+{
+
+}
+
+void Firefly::flashReceiveReset()
+{
+
+}
+
+void Firefly::flashMirolloStrogtzModel()
+{
+
+}
+
+void Firelfy::flashBuckPhaseAdvance()
+{
+
+}
+
+void Firefly::flashBuckPhaseDelay()
+{
+
+}
+
+void Firefly::receiveHandlerMirolloStrogatzModel()
+{
+
+}
+
+void Firefly::receiveHandlerBuckPhaseAdvance()
+{
+
+}
+
+void Firefly::receiveHandlerBuckPhaseDelay()
+{
+
+}
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+	
+}
