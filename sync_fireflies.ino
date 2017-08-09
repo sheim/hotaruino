548875c69b0efd925d60b494b881ca3e06b1ab2f
Jonas Lang <jonilang@gmx.de>
2017-08-09 10:22:26 +0200

    include Firefly.h and creat an object ot Firefly

--- sync_fireflies.ino
+++ sync_fireflies.ino
@@ -1,3 +1,8 @@
+#include "Firefly.h"
+
+Firefly firefly(); //creating an object of the class Firefly that includes all necessary variable and functions
+//with the creation of that object all basic Timer and Output configurations are made
+
 void void setup()
 {
 	
