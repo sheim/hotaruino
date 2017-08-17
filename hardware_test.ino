117dc75950a3dc2b4f5b8e28ea36dc944bc09ef4
Jonas Lang <jonilang@gmx.de>
2017-08-17 10:55:14 +0200

    add variables

--- hardware_test.ino
+++ hardware_test.ino
@@ -2,6 +2,15 @@
 
 Firefly firefly; //creating a new object of class Firefly
 
+double old_flash_interval = 0; //all those variables help to recognize changes in the hardware
+double old_epsilon = 0;
+
+double noise_epsilon = 0;
+double noise_flash_interval = 0;
+
+char switch_state = 0;
+char old_switch_state = 0;
+
 void setup()
 {
 
